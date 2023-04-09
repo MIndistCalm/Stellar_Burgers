f2d6c451dd7f0507284a9107675595899d98298c
MIndistCalm <suxinin5@yandex.ru>
2023-04-09 19:18:03 +0700

    fixs alignment logo

--- react-burger/src/components/BurgerConstructor/BurgerConstructor.tsx
+++ react-burger/src/components/BurgerConstructor/BurgerConstructor.tsx
@@ -1,79 +1,86 @@
-import { Button, ConstructorElement, CurrencyIcon, DragIcon } from '@ya.praktikum/react-developer-burger-ui-components'
-import React from 'react'
-import { Data } from '../../types'
+import {
+  Button,
+  ConstructorElement,
+  CurrencyIcon,
+  DragIcon,
+} from "@ya.praktikum/react-developer-burger-ui-components";
+import React from "react";
+import { Data } from "../../types";
 
-import styles from './styles.module.css'
+import styles from "./styles.module.css";
 
 interface DataProps {
-	data: Data[]
+  data: Data[];
 }
 
 export const BurgerConstructor = ({ data }: DataProps) => {
-	return (
-		<section className={`${styles.constructor_section} mt-15 ml-4 mr-4`}>
-			<article className={`${styles.constructor_container}`}>
-				<ConstructorElement
-					type='top'
-					isLocked={true}
-					text='Краторная булка N-200i (верх)'
-					price={200}
-					thumbnail={data[0].image}
-					extraClass='ml-8'
-				/>
-				<div className='constructor-drag-item'>
-					<DragIcon type='primary' />
-					<ConstructorElement
-						text='Краторная булка N-200i (верх)'
-						price={50}
-						thumbnail={data[2].image}
-					/>
-				</div>
-				<div className='constructor-drag-item'>
-					<DragIcon type='primary' />
-					<ConstructorElement
-						text='Краторная булка N-200i (верх)'
-						price={50}
-						thumbnail={data[0].image}
-					/>
-				</div>
-				<div className='constructor-drag-item'>
-					<DragIcon type='primary' />
-					<ConstructorElement
-						text='Краторная булка N-200i (верх)'
-						price={50}
-						thumbnail={data[0].image}
-					/>
-				</div>
-				<div className='constructor-drag-item'>
-					<DragIcon type='primary' />
-					<ConstructorElement
-						text='Краторная булка N-200i (верх)'
-						price={50}
-						thumbnail={data[0].image}
-					/>
-				</div>
+  return (
+    <section className={`${styles.constructor_section} mt-15 ml-4 mr-4`}>
+      <article className={`${styles.constructor_container}`}>
+        <ConstructorElement
+          type="top"
+          isLocked={true}
+          text="Краторная булка N-200i (верх)"
+          price={200}
+          thumbnail={data[0].image}
+          extraClass="ml-8"
+        />
+        <div className="constructor-drag-item">
+          <DragIcon type="primary" />
+          <ConstructorElement
+            text="Краторная булка N-200i (верх)"
+            price={50}
+            thumbnail={data[2].image}
+          />
+        </div>
+        <div className="constructor-drag-item">
+          <DragIcon type="primary" />
+          <ConstructorElement
+            text="Краторная булка N-200i (верх)"
+            price={50}
+            thumbnail={data[0].image}
+          />
+        </div>
+        <div className="constructor-drag-item">
+          <DragIcon type="primary" />
+          <ConstructorElement
+            text="Краторная булка N-200i (верх)"
+            price={50}
+            thumbnail={data[0].image}
+          />
+        </div>
+        <div className="constructor-drag-item">
+          <DragIcon type="primary" />
+          <ConstructorElement
+            text="Краторная булка N-200i (верх)"
+            price={50}
+            thumbnail={data[0].image}
+          />
+        </div>
 
-				<ConstructorElement
-					type='bottom'
-					isLocked={true}
-					text='Краторная булка N-200i (низ)'
-					price={200}
-					thumbnail={data[0].image}
-					extraClass='ml-8'
-				/>
-			</article>
-			<footer className='constructor-footer mt-10'>
-				<span className='text text_type_digits-medium mr-2'>1280</span>
-				<CurrencyIcon type='primary' />
-				<Button
-					htmlType='button'
-					type='primary'
-					size='large'
-					extraClass='ml-10'
-				>
-					Оформить заказ
-				</Button>
-			</footer>
-		</section>
-	)
-}
+        <ConstructorElement
+          type="bottom"
+          isLocked={true}
+          text="Краторная булка N-200i (низ)"
+          price={200}
+          thumbnail={data[0].image}
+          extraClass="ml-8"
+        />
+      </article>
+      <footer className="constructor-footer mt-10">
+        <div className="price-block">
+          <span className="text text_type_digits-medium mr-2">1280</span>
+          <CurrencyIcon type="primary" />
+        </div>
+        <Button
+          htmlType="button"
+          type="primary"
+          size="large"
+          extraClass="ml-10"
+        >
+          Оформить заказ
+        </Button>
+      </footer>
+    </section>
+  );
+};
